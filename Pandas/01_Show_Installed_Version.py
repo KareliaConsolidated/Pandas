dab5b5bb6716d1b671331f1adc39172fb8ec6d0f
Anubhav Karelia <kareliaanubhav@outlook.com>
2019-12-08 21:17:43 +0530

    Pandas Tricks: Show Installed Version

---
+++ Pandas/01_Show_Installed_Version.py
@@ -0,0 +1,60 @@
+from Datasets import *
+
+# Show Installed Version
+print(pd.__version__) # 0.25.1
+print(pd.show_versions()) # To Check Version of Python Dependencies
+
+# INSTALLED VERSIONS
+# ------------------
+# commit           : None
+# python           : 3.7.4.final.0
+# python-bits      : 64
+# OS               : Linux
+# OS-release       : 5.3.0-24-generic
+# machine          : x86_64
+# processor        : x86_64
+# byteorder        : little
+# LC_ALL           : None
+# LANG             : en_IN
+# LOCALE           : en_IN.ISO8859-1
+
+# pandas           : 0.25.1
+# numpy            : 1.17.2
+# pytz             : 2019.3
+# dateutil         : 2.8.0
+# pip              : 19.2.3
+# setuptools       : 41.4.0
+# Cython           : 0.29.13
+# pytest           : 5.2.1
+# hypothesis       : None
+# sphinx           : 2.2.0
+# blosc            : None
+# feather          : None
+# xlsxwriter       : 1.2.1
+# lxml.etree       : 4.4.1
+# html5lib         : 1.0.1
+# pymysql          : None
+# psycopg2         : None
+# jinja2           : 2.10.3
+# IPython          : 7.8.0
+# pandas_datareader: None
+# bs4              : 4.8.0
+# bottleneck       : 1.2.1
+# fastparquet      : None
+# gcsfs            : None
+# lxml.etree       : 4.4.1
+# matplotlib       : 3.1.1
+# numexpr          : 2.7.0
+# odfpy            : None
+# openpyxl         : 3.0.0
+# pandas_gbq       : None
+# pyarrow          : None
+# pytables         : None
+# s3fs             : None
+# scipy            : 1.3.1
+# sqlalchemy       : 1.3.9
+# tables           : 3.5.2
+# xarray           : None
+# xlrd             : 1.2.0
+# xlwt             : 1.3.0
+# xlsxwriter       : 1.2.1
