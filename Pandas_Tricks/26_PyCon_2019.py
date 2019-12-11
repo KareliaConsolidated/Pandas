e2493c5930f5c20eed3ad92e21da729aa2948a56
Anubhav Karelia <kareliaanubhav@outlook.com>
2019-12-11 22:06:38 +0530

    Completed Task of EDA

--- Pandas_Tricks/26_PyCon_2019.py
+++ Pandas_Tricks/26_PyCon_2019.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 ted = pd.read_csv('Datasets/ted.csv')
 
@@ -48,7 +49,8 @@ print(ted.dtypes)
 # views                  int64
 # dtype: object
 
-# WHICH TALKS PROVOKE THE MOST ONLINE DISCUSSION ?
+##### WHICH TALKS PROVOKE THE MOST ONLINE DISCUSSION ?
+
 # Consider the limitations and biases of your data when analyzing it.
 print(ted.sort_values('comments').tail())
 #       comments                                        description  ...                                                url     views
@@ -77,3 +79,91 @@ print(ted.sort_values('views_per_comment').head())
 # 96       6404  Richard Dawkins urges all atheists to openly s...      1750  ...  4374792           0.001464         683.134291
 # 694      1502  Filmmaker Sharmeen Obaid-Chinoy takes on a ter...       489  ...  1057238           0.001421         703.886818
 # 954      2492  Janet Echelman found her true voice as an arti...       566  ...  1832930           0.001360         735.525682
+
+##### VISUALIZE THE DISTRIBUTION OF COMMENTS
+# 1. Choose your plot type based on the question you are answering and the data type(s) you are working with.
+# 2. Use pandas as one-liners to iterate through plots quickly
+# 3. Try modifing the plot defaults
+# 4. Creating Plots invloves decision-making
+
+print(ted[ted.comments >= 1000].shape) # 32,9
+################# WAY 01 #################
+# ted[ted.comments < 1000].comments.plot(kind='hist')
+# OR
+################# WAY 02 #################
+# ted.query('comments < 1000').comments.plot(kind='hist')
+# OR
+################# WAY 03 #################
+ted.loc[ted.comments < 1000, 'comments'].plot(kind='hist',bins=20)
+
+# plt.show()
+
+# PLOT THE NUMBER OF TALKS THAT TOOK PLACE EACH YEAR
+# 1. Read the Documentation
+# 2. Use the datetime data type for dates and times
+# 3. Check your work as you go
+# 4. Consider excluding data if it might not be relevant
+print(ted.event.sample(10)) # Random 10 Samples
+print(ted.film_date.head()) # Unix TimeStamp
+# 0    1140825600
+# 1    1140825600
+# 2    1140739200
+# 3    1140912000
+# 4    1140566400
+# Name: film_date, dtype: int64
+
+print(pd.to_datetime(ted.film_date, unit='s').head())
+# 0   2006-02-25
+# 1   2006-02-25
+# 2   2006-02-24
+# 3   2006-02-26
+# 4   2006-02-22
+# Name: film_date, dtype: datetime64[ns]
+ted['film_datetime'] = pd.to_datetime(ted.film_date, unit='s')
+
+print(ted[['event','film_datetime']].sample(5))
+#                   event film_datetime
+# 2140  TEDxCreativeCoast    2015-05-01
+# 1906     TEDGlobal 2014    2014-10-06
+# 148             TED2007    2007-03-03
+# 1202         TEDxChange    2012-04-05
+# 1492            TED2013    2013-02-26
+
+print(ted.film_datetime.dt.year.head()) #dayofyear
+# 0    2006
+# 1    2006
+# 2    2006
+# 3    2006
+# 4    2006
+# Name: film_datetime, dtype: int64
+
+print(ted.film_datetime.dt.year.value_counts().sort_index().plot())
+# 2013    270
+# 2011    270
+# 2010    267
+# 2012    267
+# 2016    246
+# 2015    239
+# 2014    237
+# 2009    232
+# 2007    114
+# 2017     98
+# 2008     84
+# 2005     66
+# 2006     50
+# 2003     33
+# 2004     33
+# 2002     27
+# 1998      6
+# 2001      5
+# 1983      1
+# 1991      1
+# 1994      1
+# 1990      1
+# 1984      1
+# 1972      1
+# Name: film_datetime, dtype: int64
+
+# plt.show()
+
+##### What were the "BEST" events in TED history to attend ? 
